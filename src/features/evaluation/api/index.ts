2e1dc2fa927304663ca82b19a458e4e42dce4302
NieYing <274891993@qq.com>
2026-02-23 23:07:04 +0800

    refactor: 清理评价API的非网络请求代码

--- src/features/evaluation/api/index.ts
+++ src/features/evaluation/api/index.ts
@@ -1,111 +1,73 @@
-import {jwxt} from "@/js/jw/jwxt.ts";
 import {http, objectToFormUrlEncoded} from "@/core/http.ts";
-import {ToastAndroid} from "react-native";
 import {EvaTeacherListRes, TeacherListRes} from "@/features/evaluation/types/schema/TeacherList.ts";
 
 export const evaluationApi = {
     // 获得评价列表
-    getEvaluationList: (): Promise<EvaTeacherListRes> => {
-        return new Promise(async (resolve, reject) => {
-            if (!(await jwxt.testToken())) {
-                reject();
-                return;
-            }
-            const reqBody = objectToFormUrlEncoded({
-                queryModel: {
-                    showCount: 150,
-                },
-            });
-            const res = await http.post("/xspjgl/xspj_cxXspjIndex.html?doType=query&gnmkdm=N401605", reqBody);
-            if (typeof res.data === "object") {
-                const valiRes = TeacherListRes.safeParse(res.data);
-                if (!valiRes.success) {
-                    console.log("API 响应错误", valiRes.error);
-                    return null;
-                }
-                resolve(valiRes.data);
-            } else {
-                ToastAndroid.show("获取评价信息失败", ToastAndroid.SHORT);
-                reject(res);
-            }
+    async getEvaluationList(): Promise<EvaTeacherListRes> {
+        const reqBody = objectToFormUrlEncoded({
+            queryModel: {
+                showCount: 150,
+            },
         });
+        const res = await http.post("/xspjgl/xspj_cxXspjIndex.html?doType=query&gnmkdm=N401605", reqBody);
+
+        if (typeof res.data !== "object") {
+            throw new Error("响应格式异常");
+        }
+
+        const valiRes = TeacherListRes.safeParse(res.data);
+        if (!valiRes.success) {
+            throw new Error("数据解析失败");
+        }
+
+        return valiRes.data;
     },
     // 获得具体的HTML页面
-    getEvaluationDetail: (
+    async getEvaluationDetail(
         shaWord: string,
         classId: string,
         courseId: string,
         xsdm: string,
         pjmbmcb_id: string,
-    ): Promise<string> => {
-        return new Promise(async (resolve, reject) => {
-            if (!(await jwxt.testToken())) {
-                reject();
-                return;
-            }
-            const reqBody = objectToFormUrlEncoded({
-                jgh_id: shaWord,
-                jxb_id: classId,
-                kch_id: courseId,
-                xsdm,
-                pjmbmcb_id,
-            });
-            const res = await http.post("/xspjgl/xspj_cxXspjDisplay.html?gnmkdm=N401605", reqBody);
-            if (typeof res.data === "string") {
-                resolve(res.data);
-            } else {
-                ToastAndroid.show("获取具体评价信息失败", ToastAndroid.SHORT);
-                reject(res);
-            }
+    ): Promise<String> {
+        const reqBody = objectToFormUrlEncoded({
+            jgh_id: shaWord,
+            jxb_id: classId,
+            kch_id: courseId,
+            xsdm,
+            pjmbmcb_id,
         });
+        const res = await http.post("/xspjgl/xspj_cxXspjDisplay.html?gnmkdm=N401605", reqBody);
+        if (typeof res.data === "string") {
+            return res.data;
+        } else {
+            throw new Error("HTML获取失败");
+        }
     },
     // 只传 default 参数则会消掉所有已有的评价
-    handleEvaResult: (Params1: any, Params2?: any): Promise<string> => {
-        return new Promise(async (resolve, reject) => {
-            if (!(await jwxt.testToken())) {
-                reject();
-                return;
-            }
-            // 可选项，目前发现不提交这些也能评价成功
-            const Params3 = {
-                jszdpjbl: "0",
-                xykzpjbl: "0",
-                "modelList[0].pjzt": "0",
-                tjzt: "0",
-            };
-            const reqBody = objectToFormUrlEncoded({...Params1, ...Params2});
-            console.log(reqBody.replaceAll("&", "\n"));
-            const res = await http.post("/xspjgl/xspj_bcXspj.html?gnmkdm=N401605", reqBody);
-            if (typeof res.data === "string") {
-                // ToastAndroid.show(res.data, ToastAndroid.SHORT);
-                resolve(res.data);
-            } else {
-                ToastAndroid.show("保存失败", ToastAndroid.SHORT);
-                reject(res);
-            }
-        });
+    async handleEvaResult(Params1: any, Params2?: any): Promise<String> {
+        const Params3 = {
+            jszdpjbl: "0",
+            xykzpjbl: "0",
+            "modelList[0].pjzt": "0",
+            tjzt: "0",
+        };
+        const reqBody = objectToFormUrlEncoded({...Params1, ...Params2});
+        const res = await http.post("/xspjgl/xspj_bcXspj.html?gnmkdm=N401605", reqBody);
+        if (typeof res.data === "string") {
+            return res.data;
+        } else {
+            throw new Error("保存失败");
+        }
     },
-    submitEvaResult: (Params1: any, Params2?: any): Promise<string> => {
-        return new Promise(async (resolve, reject) => {
-            if (!(await jwxt.testToken())) {
-                reject();
-                return;
-            }
-            // 这两个参数在提交时必填
-            const Params3 = {
-                jszdpjbl: "0",
-                xykzpjbl: "0",
-            };
-            const reqBody = objectToFormUrlEncoded({...Params1, ...Params2, ...Params3});
-            console.log(reqBody.replaceAll("&", "\n"));
-            const res = await http.post("/xspjgl/xspj_tjXspj.html?gnmkdm=N401605", reqBody);
-            if (typeof res.data === "string") {
-                ToastAndroid.show(res.data, ToastAndroid.SHORT);
-                resolve(res.data);
-            } else {
-                ToastAndroid.show("保存失败", ToastAndroid.SHORT);
-                reject(res);
-            }
-        });
+    async submitEvaResult(Params1: any, Params2?: any): Promise<String> {
+        // 这两个参数在提交时必填
+        const Params3 = {
+            jszdpjbl: "0",
+            xykzpjbl: "0",
+        };
+        const reqBody = objectToFormUrlEncoded({...Params1, ...Params2, ...Params3});
+        const res = await http.post("/xspjgl/xspj_tjXspj.html?gnmkdm=N401605", reqBody);
+        return res.data;
     },
 };
