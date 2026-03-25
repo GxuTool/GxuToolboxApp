18e4dd5a0b6eadf890271dbb8dc5b280ee3fde95
NieYing <274891993@qq.com>
2026-03-25 22:44:27 +0800

    refactor: 实践课列表组件迁移到features目录，优化类型

--- src/features/courseSchedule/components/PracticalCourseList.tsx
+++ src/features/courseSchedule/components/PracticalCourseList.tsx
@@ -1,4 +1,3 @@
-import {PracticalCourse} from "@/type/infoQuery/course/course.ts";
 import {StyleSheet, View} from "react-native";
 import {BaseColor, Color} from "@/shared/color.ts";
 import Flex from "@/components/un-ui/Flex.tsx";
@@ -6,12 +5,15 @@ import {Icon} from "@/components/un-ui/Icon.tsx";
 import {Text, useTheme} from "@rneui/themed";
 import {useContext, useEffect, useState} from "react";
 import {CourseScheduleContext} from "@/js/jw/course.ts";
+import {ICourse} from "@/features/courseSchedule/type/schema/course.ts";
+
+type PracticeItem = ICourse["practiceList"][number];
 
 interface Props {
-    courseList: PracticalCourse[];
+    courseList: ICourse["practiceList"];
 }
 
-interface PracticalCourseItem extends PracticalCourse {
+interface PracticalCourseItem extends PracticeItem {
     // 在课程表中显示的背景颜色
     backgroundColor: string;
 }
@@ -42,23 +44,23 @@ const staticData = {
 
 export function PracticalCourseList(props: Props) {
     const {theme} = useTheme();
-    const [courseList, setCourseList] = useState<PracticalCourseItem[]>([]);
+    const [courseList, setCourseList] = useState<ICourse["practiceList"]>([]);
     useEffect(() => {
-        randomCourseColor(props.courseList as PracticalCourseItem[]);
-        setCourseList(props.courseList as PracticalCourseItem[]);
+        randomCourseColor(props.courseList as ICourse["practiceList"]);
+        setCourseList(props.courseList as ICourse["practiceList"]);
     }, [props.courseList]);
 
     const {courseScheduleStyle} = useContext(CourseScheduleContext)!;
 
-    function randomCourseColor(courseList: PracticalCourseItem[]) {
+    function randomCourseColor(courseList: ICourse["practiceList"]) {
         //使得相同课程的颜色相同
         const courseColor: Record<string, string> = {};
         courseList.forEach((course: PracticalCourseItem) => {
-            if (!courseColor[course.kcmc]) {
+            if (!courseColor[course.title]) {
                 let randomNum = Math.floor(Math.random() * staticData.randomColor.length);
-                course.backgroundColor = courseColor[course.kcmc] = staticData.randomColor[randomNum];
+                course.backgroundColor = courseColor[course.title] = staticData.randomColor[randomNum];
             } else {
-                course.backgroundColor = courseColor[course.kcmc];
+                course.backgroundColor = courseColor[course.title];
             }
         });
     }
@@ -79,20 +81,20 @@ export function PracticalCourseList(props: Props) {
                 });
                 return (
                     <View
-                        key={`${course.qtkcgs}-${index}`}
+                        key={`${course.title}-${index}`}
                         style={[
                             itemStyle.course,
                             courseScheduleStyle.practicalCourseItem,
                             courseScheduleStyle.courseItem,
                         ]}>
-                        <Text style={itemStyle.text}>{course.qtkcgs}</Text>
+                        <Text style={itemStyle.text}>{course.title}</Text>
                         <Flex gap={5}>
                             <Icon name="clock" style={itemStyle.text} />
-                            <Text style={itemStyle.text}>{course.qsjsz}</Text>
+                            <Text style={itemStyle.text}>{course.time}</Text>
                         </Flex>
                         <Flex gap={5}>
                             <Icon name="account" style={itemStyle.text} />
-                            <Text style={itemStyle.text}>{course.jsxm}</Text>
+                            <Text style={itemStyle.text}>{course.teacher}</Text>
                         </Flex>
                     </View>
                 );
