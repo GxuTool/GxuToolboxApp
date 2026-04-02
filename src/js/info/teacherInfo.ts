03d8257ecfb7c502a384d192c30acf8da42fed78
kogarRin <kogarning@qq.com>
2026-04-02 11:44:51 +0800

    feat：修改教师信息接口类型

--- src/js/info/teacherInfo.ts
+++ src/js/info/teacherInfo.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {BaseInfoRes, DetailInfo} from "@/type/api/teacherInfo/info.ts";
+import {BaseInfoRes, DetailTeacherInfo} from "@/type/api/teacherInfo/info.ts";
 
 const http = axios.create({
     baseURL: "https://prof.gxu.edu.cn/backend/openapi/openSearch",
@@ -8,22 +8,31 @@ const http = axios.create({
     },
 });
 
-const teacherInfoApi = {
+export const teacherInfoApi = {
+    /**
+     * 获取教师基础信息
+     * @param name 教师姓名
+     */
     getBaseInfo: async (name: string): Promise<BaseInfoRes> => {
         const res = await http.get<BaseInfoRes>("/findByCondition", {
             params: {
                 xm: name,
-                pageNum: "1",
-                pageSize: "12",
+                pageNum: 1,
+                pageSize: 100,
             },
         });
         return res.data;
     },
 
-    getDetailInfo: async (name: string, school: string): Promise<DetailInfo> => {
+    /**
+     * 获取教师详细信息
+     * @param name 教师姓名
+     * @param school 所在学院名称
+     */
+    getDetailInfo: async (name: string, school: string): Promise<DetailTeacherInfo> => {
         const baseRes = await teacherInfoApi.getBaseInfo(name);
         const teacherBaseInfo = baseRes.resData.list.find(item => item.dwmc === school);
-        const res = await http.get<DetailInfo>("/getTeachInfoByGH", {
+        const res = await http.get<DetailTeacherInfo>("/getTeachInfoByGH", {
             params: {
                 GH: teacherBaseInfo?.GH ?? "",
             },
